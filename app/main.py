bd42c66131b134e20c00d8cac8db077ee57658e3
Denys Halytskyi <denys.halytskyi17@gmail.com>
2025-02-23 20:26:28 +0200

    S

--- app/main.py
+++ app/main.py
@@ -58,8 +58,6 @@ class DeliveryDrone(FlyingRobot):
     def hook_load(self, cargo: Cargo) -> None:
         if self.current_load is None and cargo.weight <= self.max_load_weight:
             self.current_load = cargo
-        else:
-            print("Cargo exceeds maximum load weight")
 
     def unhook_load(self) -> None:
         self.current_load = None
